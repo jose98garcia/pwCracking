baedc115401a05e4e0fc82320824e62762083408
Jose Garcia <garcia.jose804@gmail.com>
2019-03-25 17:35:22 -0500

    setup for program

---
+++ pCracker.py
@@ -0,0 +1,63 @@
+#Four Methods for Password Cracking
+
+#Menu for testing
+def menu():
+    print("Choose a method for password cracking")
+    print("-------------------------------------")
+    print("1. Password with only lowercase letters")
+    print("2. Password with lower and uppercase letters")
+    print("3. Password with lower, uppercase letters, and digits")
+    print("4. Password with lower, uppercase letters, digits, and symbols")
+
+    option = input()
+    option = int(option)
+
+    while((option < 1) or (option > 4)):
+        print("That is not an option. Try again.")
+        option = input()
+        option = int(option)
+
+    return option
+
+#---------------------------------------------------------------------------------------------
+
+#Takes you to the right option from menu
+def chooseMethod(num):
+    if(num == 1):
+        methodOne()
+    elif(num ==2):
+        methodTwo()
+    elif(num == 3):
+        methodThree()
+    elif(num == 4):
+        methodFour()
+    else:
+        print("Invalid method... something went wrong")
+
+
+#---------------------------------------------------------------------------------------------
+#Password with only lowercase letters
+def methodOne():
+    print("Method one")
+
+#Password with lower and uppercase letters
+def methodTwo():
+    print("Method two")
+
+#Password with lower, uppercase letters, and digits
+def methodThree():
+    print("Method three")
+
+#Password with lower, uppercase letters, digits, and symbols
+def methodFour():
+    print("Method four")
+
+
+#---------------------------------------------------------------------------------------------
+#Main Program
+number = menu()
+chooseMethod(number)
+
+
+
+    
